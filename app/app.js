dc523feaccfd09f02107f7ef3910a8baed4703cb
Mayank kumar singh <mayank.reactjs@gmail.com>
2024-12-19 22:25:15 +0530

    isLogin:middleware added

--- app/app.js
+++ app/app.js
@@ -10,6 +10,28 @@ app.use(morgan("dev"));
 //
 //Routes
 //
+app.use((req, res, next) => {
+  console.log(`${req.method}${req.originalUrl}'`);
+  next();
+});
+//
+let user = {
+  name: "Mayank",
+  isAdmin: false,
+  isLoggedIn: false,
+};
+//
+const isLogin = (req, res, next) => {
+  if (user.isLoggedIn) {
+    next();
+  } else {
+    res.status(401).json({
+      message: "Unauthorized!",
+    });
+  }
+};
+//
+app.use(isLogin);
 //
 app.use("/api/v1/admins", adminRouter);
 //
