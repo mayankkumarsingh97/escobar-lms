154b33d7a68da6c909fa9a7b50a09751d970d22d
Mayank kumar singh <66870313+mayankkumarsingh97@users.noreply.github.com>
2025-05-01 14:48:32 +0530

    Update adminCtrl.js

--- controllers/staff/adminCtrl.js
+++ controllers/staff/adminCtrl.js
@@ -138,7 +138,8 @@ exports.adminPublishTeacherCtrl = asyncHandler((req, res) => {
 // @ access Private
 exports.adminUnPublishTeacherCtrl = asyncHandler((req, res) => {
   res.json({
-    status: "success",
+    status: "success!",
     data: "Admin un-publish results ",
+    error:false
   });
 });
